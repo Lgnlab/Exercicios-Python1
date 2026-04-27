4675ea9ec2887344f80af0a95e761dc69132c475
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-04-27 15:28:54 -0300

    .

--- desafios/016.py
+++ desafios/016.py
@@ -7,7 +7,10 @@ class Funcionario:
         self.cargo = cargo
 
     def apresentar(self):
-        return f':hand: Olá, sou [blue bold]{self.nome}[/] e sou [yellow bold]{self.cargo}[/] do setor de [red bold]{self.setor}[/]'
+        return f':hand: Olá, sou [blue bold]{self.nome}[/] e sou [yellow bold]{self.cargo}[/] do setor de [red bold]{self.setor}[/] da empresa Lucas S.A'
 
 c1 = Funcionario('Lucas', 'TI', 'Programador')
 print(c1.apresentar())
+
+c2 = Funcionario('Maria', 'Administração', 'Diretora')
+print(c2.apresentar())
