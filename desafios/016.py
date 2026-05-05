23061d207f2c3f62854d7da133c30426ae12ddc2
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-05-05 11:54:19 -0300

    .

--- desafios/016.py
+++ desafios/016.py
@@ -1,13 +1,15 @@
 from rich import print
 
 class Funcionario:
+    # Atributos de classe
+    empresa = 'Lucas S.A'
     def __init__(self, nome, setor, cargo):
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
 
     def apresentar(self):
-        return f':hand: Olá, sou [blue bold]{self.nome}[/] e sou [yellow bold]{self.cargo}[/] do setor de [red bold]{self.setor}[/] da empresa Lucas S.A'
+        return f':hand: Olá, sou [blue bold]{self.nome}[/] e sou [yellow bold]{self.cargo}[/] do setor de [red bold]{self.setor}[/] da empresa {Funcionario.empresa}'
 
 c1 = Funcionario('Lucas', 'TI', 'Programador')
 print(c1.apresentar())
