82c5e7b8baeed50a1688eccfa757e836136bff82
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-05-08 13:46:00 -0300

    .

---
+++ exercicios/ex005/__main__.py
@@ -0,0 +1,16 @@
+from rich import print, inspect
+from classesex005 import Aluno, Professor, Funcionario
+
+a1 = Aluno("José", 17, "Informática", "T01")
+a1.fazer_aniversario()
+a1.fazer_matricula()
+
+
+p1 = Professor("Samuel", 37, "Biologia", "Mestrado")
+p1.fazer_aniversario()
+p1.dar_aula()
+
+
+f1 = Funcionario("Lucas", 25, "Programador", "TI")
+f1.fazer_aniversario()
+f1.bater_ponto()
